454177bed8e17c52ef6231b4d95644f3ce10930e
CAndrei1601 <chioreanandrei@users.noreply.github.com>
2020-09-20 16:08:33 +0300

    Updates

---
+++ precache-manifest.6d6d270ee17b49adefc8ee753734b09c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3d01cbc9f656af593e8f616960931d",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "aa1fe314069758cbeaff",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "96760ee967e8c1ba0c8a",
+    "url": "/Robots/static/css/main.4e7d2d1f.chunk.css"
+  },
+  {
+    "revision": "aa1fe314069758cbeaff",
+    "url": "/Robots/static/js/2.30b70ab8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.30b70ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96760ee967e8c1ba0c8a",
+    "url": "/Robots/static/js/main.fc46cdda.chunk.js"
+  },
+  {
+    "revision": "6b87c073089052519813",
+    "url": "/Robots/static/js/runtime-main.276f5916.js"
+  }
+]);
